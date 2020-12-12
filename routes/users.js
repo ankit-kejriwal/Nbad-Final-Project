36992b1e983862a9ae16674736118cef11515e27
ankit <ankitkejriwaal@gmail.com>
2020-12-12 16:22:01 -0500

    added register api to the codebase

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const passport =  require('passport');
+const jwt =  require('jsonwebtoken');
+const config = require('../config/database')
+const User = require("../models/User");
 
 
 // Register
-router.get('/register',(req,res,next) => {
-  res.send('Register');
+router.post('/register',(req,res,next) => {
+    let newUser  = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+      });
+    
+      User.addUser(newUser,(err,user) => {
+        if(err){
+          res.json({ success: false, msg: "Failed to register user" });
+        } else {
+          res.json({ success: true, msg: "user Registered" });
+        }
+      });
 });
 
 // Authenticate
@@ -17,9 +34,4 @@ router.get('/profile',(req,res,next) => {
     res.send('Profile');
   });
 
-// Validate
-router.get('/validate',(req,res,next) => {
-    res.send('Validate');
-  });
-
-  module.exports = router;
+module.exports = router;
