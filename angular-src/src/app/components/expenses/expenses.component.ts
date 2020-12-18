d8a34d64bc6aff992daab1736961bc02a7ba3169
ankit <ankitkejriwaal@gmail.com>
2020-12-17 20:14:00 -0500

    added calendar

--- angular-src/src/app/components/expenses/expenses.component.ts
+++ angular-src/src/app/components/expenses/expenses.component.ts
@@ -5,32 +5,43 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
-  styleUrls: ['./expenses.component.scss']
+  styleUrls: ['./expenses.component.scss'],
 })
 export class ExpensesComponent implements OnInit {
   categories = [];
+  dp: any;
   constructor(
     private commonService: CommonService,
-    private toastr: ToastrService,
-  ) { }
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.getAllCategory();
   }
 
-  getAllCategory(){
-    this.commonService.getAllCategory().
-      subscribe((data: any) => {
-          if(data.length  > 0){
-            for(let i = 0;i<data.length;i++){
-              this.categories.push(data[i]);
-            }
+  getAllCategory() {
+    this.dp = $('#datepicker').datepicker({
+      format: 'mm-yyyy',
+      startView: 'months',
+      minViewMode: 'months',
+      autoclose: true,
+    }).on('changeDate', function(selected){
+      console.log(selected);
+  });
+    this.commonService.getAllCategory().subscribe(
+      (data: any) => {
+        if (data.length > 0) {
+          for (let i = 0; i < data.length; i++) {
+            this.categories.push(data[i]);
           }
-          console.log(this.categories);
+        }
+        console.log(this.categories);
       },
-      (err) =>{
-        this.toastr.error('Unable to fetch category', 'Error',{timeOut: 3000});
-      });
+      (err) => {
+        this.toastr.error('Unable to fetch category', 'Error', {
+          timeOut: 3000,
+        });
+      }
+    );
   }
-
 }
